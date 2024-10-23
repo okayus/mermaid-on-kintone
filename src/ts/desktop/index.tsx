0f9532ed451b4a010dac1c2da74b423c647ae4dc
okayus <toshiaki.mukai.9981@gmail.com>
2024-10-23 13:47:25 +0900

    feat:レコード詳細、編集画面でMermaid図を表示する

--- src/ts/desktop/index.tsx
+++ src/ts/desktop/index.tsx
@@ -1,22 +1,60 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { createRoot } from "react-dom/client";
+
+declare const mermaid: any;
+interface MermaidViewerProps {
+  code: string;
+}
+
+const MermaidViewer: React.FC<MermaidViewerProps> = ({ code }) => {
+  const mermaidRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    if (mermaidRef.current) {
+      try {
+        mermaid.initialize({ startOnLoad: false });
+        mermaidRef.current.innerHTML = code;
+        mermaid.init(undefined, mermaidRef.current);
+      } catch (error) {
+        console.error("Mermaidの描画に失敗しました:", error);
+        mermaidRef.current.innerHTML = "<p>Mermaidの描画に失敗しました</p>";
+      }
+    }
+  }, [code]);
+
+  return <div ref={mermaidRef} />;
+};
 
 ((PLUGIN_ID) => {
   kintone.events.on(
-    ["app.record.create.show", "app.record.edit.show"],
+    ["app.record.detail.show", "app.record.edit.show"],
     (event) => {
-      const headerMenuSpace = kintone.app.record.getHeaderMenuSpaceElement();
-      if (!headerMenuSpace) {
+      const responseConfig = kintone.plugin.app.getConfig(PLUGIN_ID);
+      if (!responseConfig.config) {
         return event;
       }
-      const root = createRoot(headerMenuSpace);
-      root.render(
-        <div>
-          <h1>Hello, World!</h1>
-          <p>Plugin ID: {PLUGIN_ID}</p>
-        </div>
-      );
+      const config = JSON.parse(responseConfig.config).config;
+
+      config.settings.forEach((setting: any) => {
+        const displaySpace = kintone.app.record.getSpaceElement(
+          setting.displaySpace,
+        );
+        if (!displaySpace) {
+          return;
+        }
+        displaySpace.classList.add("mermaid");
+
+        const mermaidCode = event.record[setting.inputField].value;
+
+        const root = createRoot(displaySpace);
+        root.render(
+          <div>
+            <MermaidViewer code={mermaidCode} />
+          </div>,
+        );
+      });
+
       return event;
-    }
+    },
   );
 })(kintone.$PLUGIN_ID);
